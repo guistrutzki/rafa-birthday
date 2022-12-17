5be5908b37cdb2bdaf5e955149ca793f7e88f7f2
Guillherme Strutzki <guilhermestrutzk@hotmail.com>
2022-12-17 17:58:12 -0300

    fix build error

--- src/@types/styled-components.d.ts
+++ src/@types/styled-components.d.ts
@@ -3,6 +3,5 @@ import { defaultTheme } from '../common/theme/theme'
 type Theme = typeof defaultTheme
 
 declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends Theme {}
 }
